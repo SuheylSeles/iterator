8775bee191eb85495bc2f95e1b1b14e9f82f80be
Süheyl <suheylseles@gmail.com>
2021-08-22 23:30:35 +0300

    iterator uygulaması eklendi.

---
+++ iterator.py
@@ -0,0 +1,45 @@
+class Kuvvet():
+
+    def __init__(self,max = 0):
+
+        self.max = max
+
+        self.kuvvet = 0
+
+    def __iter__(self):
+
+        return self
+
+    def __next__(self):
+
+        if(self.kuvvet <= self.max):
+
+            sonuc = 3 ** self.kuvvet
+
+            self.kuvvet += 1
+
+            return sonuc
+
+        else:
+
+            self.kuvvet = 0
+
+            raise StopIteration
+
+kuvvet = Kuvvet(6)
+
+iterator = iter(kuvvet)
+
+print(next(iterator))
+
+
+for i in kuvvet:
+
+    print(i)
+
+print("*****************")
+
+for j in kuvvet:
+
+    print(j)
+
